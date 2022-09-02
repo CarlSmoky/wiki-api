3cc02a7312fe1baceb6c45201d091df4bc0e4f65
tsumitakaoru <k.tsumita01@gmail.com>
2022-09-02 16:28:01 -0400

    Refactor using app.route

--- app.js
+++ app.js
@@ -10,50 +10,51 @@ app.set("view engine", "ejs");
 
 mongoose.connect(`mongodb://localhost:27017/wikiDB`);
 
-const articlesSchema = new mongoose.Schema ({
+const articlesSchema = new mongoose.Schema({
   title: String,
   content: String
 });
 
 const Article = mongoose.model("article", articlesSchema);
 
-app.get("/articles", (req, res) => {
-  Article.find({}, (err, allArticles) => {
-    if(!err) {
-      res.send(allArticles);
-    } else {
-      res.send(err);
-    }
+app.route('/articles')
+  .get((req, res) => {
+    Article.find({}, (err, allArticles) => {
+      if (!err) {
+        res.send(allArticles);
+      } else {
+        res.send(err);
+      }
+    })
   })
-})
 
-app.post("/articles", (req, res) => {
-  const title = req.body.title;
-  const content = req.body.content;
+  .post((req, res) => {
+    const title = req.body.title;
+    const content = req.body.content;
 
-  const article = new Article ({
-    title,
-    content
-  });
-  
-  article.save(err => {
-    if (!err) {
-      res.send("Successfully add a new article")
-    } else {
-      res.send(err);
-    }
-  });
-})
+    const article = new Article({
+      title,
+      content
+    });
 
-app.delete("/articles", (req, res) => {
-  Article.deleteMany(err =>{
-    if (!err) {
-      res.send("Successfully delete all articles");
-    } else {
-      res.send(err);
-    }
+    article.save(err => {
+      if (!err) {
+        res.send("Successfully add a new article")
+      } else {
+        res.send(err);
+      }
+    });
   })
-})
+
+  .delete((req, res) => {
+    Article.deleteMany(err => {
+      if (!err) {
+        res.send("Successfully delete all articles");
+      } else {
+        res.send(err);
+      }
+    });
+  })
 
 let port = process.env.PORT;
 if (port == null || port == '') {
